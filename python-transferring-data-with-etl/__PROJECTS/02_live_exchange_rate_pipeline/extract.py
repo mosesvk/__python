ae6798a6c868212350734fcf53fe47e58b21e185
mosesvk <kaumatule93@gmail.com>
2026-04-23 20:43:00 -0600

    update

---
+++ python-transferring-data-with-etl/__PROJECTS/02_live_exchange_rate_pipeline/extract.py
@@ -0,0 +1,23 @@
+import requests
+import pandas as pd 
+from datetime import date
+
+def extract() -> pd.DataFrame:
+    url = "https://open.er-api.com/v6/latest/USD"
+
+    res = requests.get(url)
+    res.raise_for_status()
+
+    data = res.json()
+    print(f"✅ API responded — base: {data['base']}, date: {data['date']}")
+
+    rates = data["rates"]
+    df = pd.DataFrame(rates.items(), columns=["currency", "rate"])
+    df["base_currency"] = data['base']
+    df['extracted_date'] = date.today().isoformat()
+
+    print(df.head())
+    return df
+
+if __name__ == '__main__':
+    extract()
